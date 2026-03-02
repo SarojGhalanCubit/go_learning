04cec14152a585dfa6f7555c0ff666e09566f326
awakeneddev <awakeneddevs@gmail.com>
2026-03-02 09:41:12 +0545

    Standard Response Structure

---
+++ internal/utils/response.go
@@ -0,0 +1,43 @@
+package utils
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type APIResponse struct {
+	Success bool `json:"success"`
+	Status int `json:"status"`
+	Message string `json:"message"`
+	Data interface{} `json:"data,omitempty"`
+	Error interface{} `json:"error,omitempty"`
+}
+
+
+func WriteSuccess(w http.ResponseWriter, status int, message string, data interface{}){
+	w.Header().Set("Content-Type","application/json")
+	w.WriteHeader(status)
+	
+	response := APIResponse{
+		Success: true,
+		Status: status,
+		Message: message,
+		Data: data,
+	}
+
+	json.NewEncoder(w).Encode(response)
+}
+
+func WriteError(w http.ResponseWriter, status int, message string, err interface{}) {
+	w.Header().Set("Content-Type","application/json")
+	w.WriteHeader(status)
+
+	response := APIResponse{
+		Success: false,
+		Status: status,
+		Message: message,
+		Error: err,
+	}
+
+	json.NewEncoder(w).Encode(response)
+}
