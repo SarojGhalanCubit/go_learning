11b20a0350dd4c051ee77e4cc4fb348a77abb01c
awakeneddev <awakeneddevs@gmail.com>
2026-03-02 09:42:36 +0545

    isEmail and isPhone verified user migration file

---
+++ migrations/000005_add_email_and_phone_verify_users.down.sql
@@ -0,0 +1,3 @@
+ALTER TABLE users
+DROP COLUMN is_email_verified,
+DROP COLUMN is_phone_verified;
---
+++ migrations/000005_add_email_and_phone_verify_users.up.sql
@@ -0,0 +1,3 @@
+ALTER TABLE users
+ADD COLUMN is_email_verified BOOLEAN DEFAULT FALSE,
+ADD COLUMN is_phone_verified BOOLEAN DEFAULT FALSE;
