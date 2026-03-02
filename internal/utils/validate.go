264d0fe899c1d25200a0dc9b87ac8ceac61a80ec
awakeneddev <awakeneddevs@gmail.com>
2026-03-02 09:41:24 +0545

    user validation logic

---
+++ internal/utils/validate.go
@@ -0,0 +1,49 @@
+package utils
+
+import (
+	"regexp"
+	"strings"
+)
+
+var emailRegex = regexp.MustCompile(`^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$`)
+
+func ValidateUser(name string,age int,phone string, email string, password string) map[string]string {
+	errors := make(map[string]string)
+
+	// Name validation
+	if strings.TrimSpace(name) == "" {
+		errors["name"] = "Name is required"
+	} else if len(strings.TrimSpace(name)) < 2 {
+		errors["name"] = "Name must be at least 2 characters"
+	}
+
+	// Age validation
+	if age < 18 {
+		errors["age"] = "Age must be greater than 18"
+	}
+
+	// Phone Validation
+	if strings.TrimSpace(phone) == "" {
+		errors["phone_number"] = "Phone Number is required."
+	}
+
+	// Email validation
+	if strings.TrimSpace(email) == "" {
+		errors["email"] = "Email is required"
+	} else if !emailRegex.MatchString(email) {
+		errors["email"] = "Invalid email format"
+	}
+
+	// Password validation
+	if strings.TrimSpace(password) == "" {
+		errors["password"] = "Password is required"
+	} else if len(password) < 8 {
+		errors["password"] = "Password must be at least 8 characters"
+	} else if !strings.ContainsAny(password, "0123456789") {
+		errors["password"] = "Password must contain at least one number"
+	} else if !strings.ContainsAny(password, "!@#$%^&*") {
+		errors["password"] = "Password must contain at least one special character (!@#$%^&*)"
+	}
+
+	return errors
+}
