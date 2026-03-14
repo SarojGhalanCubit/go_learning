08d7f1c94d00a2baafe14583999b9920faaafb16
awakeneddev <awakeneddevs@gmail.com>
2026-03-14 12:17:16 +0545

    extract role id from token utils created

--- internal/utils/jwt.go
+++ internal/utils/jwt.go
@@ -11,9 +11,10 @@ import (
 var jwtSecret = []byte(config.GetJwtSecretKey())
 
 
-func GenerateToken(userID int) (string, error) {
+func GenerateToken(userID,roleID int) (string, error) {
 	claims := jwt.MapClaims{
 		"user_id": userID,
+		"role_id": roleID,
 		"exp": time.Now().Add(time.Hour * 24).Unix(),
 	}
 
@@ -41,4 +42,25 @@ func GetUserIDFromToken(tokenString string) (int, error) {
     }
 
     return int(userIDFloat), nil // ✅ convert float64 → int
+}
+func GetRoleIDFromToken(tokenString string) (int, error) {
+    token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+        return jwtSecret, nil
+    })
+    if err != nil {
+        return 0, err
+    }
+
+    claims, ok := token.Claims.(jwt.MapClaims)
+    if !ok || !token.Valid {
+        return 0, errors.New("invalid token")
+    }
+
+    // ✅ MapClaims stores numbers as float64
+    roleIDFloat, ok := claims["role_id"].(float64)
+    if !ok {
+        return 0, errors.New("invalid role in token")
+    }
+
+    return int(roleIDFloat), nil // ✅ convert float64 → int
 }
