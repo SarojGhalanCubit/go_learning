1624a9c288cd3aeca82a5f4dc0860c24c0111973
awakeneddev <awakeneddevs@gmail.com>
2026-03-29 22:07:38 +0545

    jwt updated

--- internal/utils/jwt.go
+++ internal/utils/jwt.go
@@ -10,57 +10,56 @@ import (
 
 var jwtSecret = []byte(config.GetJwtSecretKey())
 
-
-func GenerateToken(userID,roleID int) (string, error) {
+func GenerateToken(userID, roleID int) (string, error) {
 	claims := jwt.MapClaims{
 		"user_id": userID,
 		"role_id": roleID,
-		"exp": time.Now().Add(time.Hour * 24).Unix(),
+		"exp":     time.Now().Add(time.Hour * 24).Unix(),
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	return  token.SignedString(jwtSecret)
+	return token.SignedString(jwtSecret)
 }
 
 func GetUserIDFromToken(tokenString string) (int, error) {
-    token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-        return jwtSecret, nil
-    })
-    if err != nil {
-        return 0, err
-    }
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return jwtSecret, nil
+	})
+	if err != nil {
+		return 0, err
+	}
 
-    claims, ok := token.Claims.(jwt.MapClaims)
-    if !ok || !token.Valid {
-        return 0, errors.New("invalid token")
-    }
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok || !token.Valid {
+		return 0, errors.New("invalid token")
+	}
 
-    // ✅ MapClaims stores numbers as float64
-    userIDFloat, ok := claims["user_id"].(float64)
-    if !ok {
-        return 0, errors.New("invalid user_id in token")
-    }
+	// ✅ MapClaims stores numbers as float64
+	userIDFloat, ok := claims["user_id"].(float64)
+	if !ok {
+		return 0, errors.New("invalid user_id in token")
+	}
 
-    return int(userIDFloat), nil // ✅ convert float64 → int
+	return int(userIDFloat), nil // ✅ convert float64 → int
 }
 func GetRoleIDFromToken(tokenString string) (int, error) {
-    token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-        return jwtSecret, nil
-    })
-    if err != nil {
-        return 0, err
-    }
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return jwtSecret, nil
+	})
+	if err != nil {
+		return 0, err
+	}
 
-    claims, ok := token.Claims.(jwt.MapClaims)
-    if !ok || !token.Valid {
-        return 0, errors.New("invalid token")
-    }
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok || !token.Valid {
+		return 0, errors.New("invalid token")
+	}
 
-    // ✅ MapClaims stores numbers as float64
-    userIDFloat, ok := claims["role_id"].(float64)
-    if !ok {
-        return 0, errors.New("invalid user_id in token")
-    }
+	// ✅ MapClaims stores numbers as float64
+	userIDFloat, ok := claims["role_id"].(float64)
+	if !ok {
+		return 0, errors.New("invalid role_id in token")
+	}
 
-    return int(userIDFloat), nil // ✅ convert float64 → int
+	return int(userIDFloat), nil // ✅ convert float64 → int
 }
