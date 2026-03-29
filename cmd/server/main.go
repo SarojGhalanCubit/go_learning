76f2629d78cf281055bf9f2e677a19f5e7e5db29
awakeneddev <awakeneddevs@gmail.com>
2026-03-29 22:06:02 +0545

    made neat and clean

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,14 +2,10 @@ package main
 
 import (
 	"context"
-	"github.com/go-chi/chi/v5"
-	chiMiddleware "github.com/go-chi/chi/v5/middleware" // optional for logging
+	"go-minimal/internal/app"
+	"go-minimal/internal/app/router"
 	"go-minimal/internal/config"
 	"go-minimal/internal/constants"
-	"go-minimal/internal/handler"
-	myMw "go-minimal/internal/middleware"
-	"go-minimal/internal/repository"
-	"go-minimal/internal/service"
 	"log"
 	"net/http"
 )
@@ -22,65 +18,16 @@ func main() {
 	defer db.Close(context.Background())
 	constants.LoadRoles()
 
-	// Initialize repo, service, handler
-	repo := repository.NewUserRepository(db)
-	materialRepo := repository.NewMaterialRepository(db)
+	// Creating Application Container
+	deps := app.NewApp(db)
 
-	// Initialize Service
-	userService := service.NewUserService(repo)
-	materialService := service.NewMaterialService(materialRepo)
-
-	userHandler := handler.NewUserHandler(userService)
-	materialHandler := handler.NewMaterialHandler(materialService)
-
-	// Initialize chi router
-	r := chi.NewRouter()
-
-	r.Use(chiMiddleware.Recoverer) // Recovers from panics
-	r.Use(chiMiddleware.RequestID)
-	r.Use(chiMiddleware.RealIP)
-	r.Use(chiMiddleware.Logger)            // logs each request
-	r.Use(chiMiddleware.Timeout(60 * 1e9)) // 60s timeout
-
-	// Public routes
-	r.Post("/login", userHandler.Login)
-
-	// Protected Routes
-	r.Group(func(r chi.Router) {
-		r.Use(myMw.AuthMiddleware) // all routes here require auth
-
-		// Admin Only routes
-		r.Group(func(r chi.Router) {
-			r.Use(myMw.RequireRole(constants.AdminID))
-			r.Put("/user/{id}/update", userHandler.UpdateUser)
-			r.Delete("/user/{id}/delete", userHandler.DeleteUser)
-
-			r.Put("/material/{id}/update", materialHandler.UpdateMaterial)
-			r.Delete("/material/{id}/delete", materialHandler.DeleteMaterial)
-
-		})
-
-		// Admin And Manager Routes
-		r.Group(func(r chi.Router) {
-			r.Use(myMw.RequireRole(constants.AdminID, constants.ManagerID))
-
-			// user endpoints
-			r.Get("/users/getAll", userHandler.GetUsers)
-			r.Get("/user/{id}/getById", userHandler.GetUserByID)
-			r.Post("/user/create", userHandler.CreateUser)
-
-			// Material endpoints
-			r.Get("/materials/getAll", materialHandler.GetAllMaterial)
-			r.Get("/material/{id}/getById", materialHandler.GeyByMaterialID)
-			r.Post("/material/create", materialHandler.CreateMaterial)
-		})
-
-	})
-
-	log.Println("Server Running on http://localhost:8082")
+	// initializingglobal router
+	r := router.NewRouter(deps)
 
 	port := config.GetPort() // change port
-	err := http.ListenAndServe(port, r)
+
+	log.Println("Server Running on http://localhost:", port)
+	err := http.ListenAndServe(port, r.Handler())
 	if err != nil {
 		log.Println("Server failed to start:", err)
 	}
