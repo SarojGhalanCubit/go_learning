674b9c4b1dee64d986400a61a20367c543a46690
awakeneddev <awakeneddevs@gmail.com>
2026-03-30 19:51:30 +0545

    user model updated

--- internal/modules/users/model/model.go
+++ internal/modules/users/model/model.go
@@ -1,20 +1,26 @@
 package model
 
+import "time"
+
 type User struct {
-	ID       int    `json:"id"`
-	Name     string `json:"name"`
-	Age      int    `json:"age"`
-	Email    string `json:"email"`
-	Phone    string `json:"phone_number"`
-	RoleID   int    `json:"role_id"`
-	Password string `json:"password"` // hide from returned JSON
+	ID        int       `json:"id"`
+	Name      string    `json:"name"`
+	Age       int       `json:"age"`
+	Email     string    `json:"email"`
+	Phone     string    `json:"phone_number"`
+	RoleID    int       `json:"role_id"`
+	Password  string    `json:"password"` // hide from returned JSON
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
 
 type UserResponse struct {
-	ID     int    `json:"id"`
-	Name   string `json:"name"`
-	Age    int    `json:"age"`
-	Email  string `json:"email"`
-	Phone  string `json:"phone_number"`
-	RoleID int    `json:"role_id"`
+	ID        int       `json:"id"`
+	Name      string    `json:"name"`
+	Age       int       `json:"age"`
+	Email     string    `json:"email"`
+	Phone     string    `json:"phone_number"`
+	RoleID    int       `json:"role_id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
