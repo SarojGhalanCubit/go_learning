b3a01e69919c58049f7459f42c24e0539915c04a
awakeneddev <awakeneddevs@gmail.com>
2026-04-08 21:55:19 +0545

    material and category repo initialized on product initialization

--- internal/app/wire_products.go
+++ internal/app/wire_products.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	categoriesRepo "go-minimal/internal/modules/categories/repository"
+	materialRepository "go-minimal/internal/modules/materials/repository"
 	productHandler "go-minimal/internal/modules/products/handler"
 	productRepo "go-minimal/internal/modules/products/repository"
 	productService "go-minimal/internal/modules/products/service"
@@ -9,7 +11,9 @@ import (
 )
 
 func (a *App) initProducts(db *pgx.Conn) {
+	catRepo := categoriesRepo.NewCategoriesRepo(db)
+	matRepo := materialRepository.NewMaterialRepository(db)
 	repo := productRepo.NewProductRepo(db)
-	svc := productService.NewProductService(repo)
+	svc := productService.NewProductService(repo, catRepo, matRepo)
 	a.ProductsHandler = productHandler.NewProductHandler(svc)
 }
