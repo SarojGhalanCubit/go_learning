316ac8b699a184ed31fe77c9d3b01cb034628d97
awakeneddev <awakeneddevs@gmail.com>
2026-04-08 22:00:12 +0545

    material and category repo added on product struct

--- internal/modules/products/service/service.go
+++ internal/modules/products/service/service.go
@@ -2,11 +2,15 @@ package productService
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	categoriesRepo "go-minimal/internal/modules/categories/repository"
 	materialRepository "go-minimal/internal/modules/materials/repository"
 	productModel "go-minimal/internal/modules/products/model"
 	productRepo "go-minimal/internal/modules/products/repository"
 	"go-minimal/internal/utils"
+	"log"
+	"strings"
 )
 
 type ProductService struct {
@@ -32,7 +36,16 @@ func (s *ProductService) GetAllProducts(ctx context.Context) ([]productModel.Pro
 }
 
 func (s *ProductService) CreateProduct(ctx context.Context, req productModel.CreateProduct) (productModel.ProductResponse, error) {
-	materialFinded, err := s.materialRepo.FindByMaterialID(ctx, req.MaterialID.String)
+	_, err := s.materialRepo.FindByMaterialID(ctx, req.MaterialID.String())
+	if err != nil {
+		return productModel.ProductResponse{}, errors.New("invalid material: material does not exist")
+	}
+
+	_, catErr := s.categoryRepo.FindByCategoryID(ctx, req.CategoryID.String())
+	if catErr != nil {
+		return productModel.ProductResponse{}, errors.New("invalid category: category does not exist")
+	}
+
 	slug, err := utils.GenerateUniqueSlug(req.Name)
 	if err != nil {
 		return productModel.ProductResponse{}, err
@@ -41,4 +54,41 @@ func (s *ProductService) CreateProduct(ctx context.Context, req productModel.Cre
 	req.Slug = slug
 
 	return s.repo.CreateProduct(ctx, req)
+}
+
+func (s *ProductService) UpdateProductByID(ctx context.Context, product productModel.CreateProduct, productID string) (productModel.ProductResponse, error) {
+	productFinded, err := s.repo.GetByID(ctx, productID)
+
+	if err != nil {
+		return productModel.ProductResponse{}, errors.New("invalid product: product does not exist")
+	}
+
+	_, materialErr := s.materialRepo.FindByMaterialID(ctx, product.MaterialID.String())
+	if materialErr != nil {
+		return productModel.ProductResponse{}, errors.New("invalid material: material does not exist")
+	}
+
+	_, catErr := s.categoryRepo.FindByCategoryID(ctx, product.CategoryID.String())
+	if catErr != nil {
+		return productModel.ProductResponse{}, errors.New("invalid category: category does not exist")
+	}
+
+	nameChange := product.Name != productFinded.Name
+	slugIsMissing := strings.TrimSpace(productFinded.Slug) == ""
+
+	if nameChange || slugIsMissing {
+		uniqueSlug, err := utils.GenerateUniqueSlug(product.Name)
+
+		if err != nil {
+			return productModel.ProductResponse{}, fmt.Errorf("failed to generate slug: %w", err)
+		}
+
+		product.Slug = uniqueSlug
+	} else {
+		product.Slug = productFinded.Slug
+	}
+	log.Println("product slug :: ", product)
+
+	return s.repo.UpdateProductByID(ctx, product, productFinded.ID.String())
+
 }
